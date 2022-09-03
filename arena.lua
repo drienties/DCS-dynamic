dbf5157af7db7977e15c409dc12924f13e927ca0
drienties <drienties@gmail.com>
2022-09-03 17:08:38 +0200

    fixed include files

--- arena.lua
+++ arena.lua
@@ -1,6 +1,3 @@
-package.path  = package.path..";"..lfs.currentdir().."/LuaSocket/?.lua"
-package.cpath = package.cpath..";"..lfs.currentdir().."/LuaSocket/?.dll"
-
 --type of Air defence units
 AD_types = {
  'RED SAM',
